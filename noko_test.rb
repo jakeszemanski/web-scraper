6e28a95d69a8aeac6e1e1b5f9a8d68270cd3f0e3
Steven Justin <sjustin@groupon.com>
2017-02-08 16:47:44 -0600

    Helped with the thing

--- noko_test.rb
+++ noko_test.rb
@@ -7,16 +7,16 @@ doc = Nokogiri::HTML(open('http://www.emptybottle.com/full/'))
 # doc.xpath('//a[@class="show_full').each do |headline|
 #   puts healine.text
 
-shows = []
-
-doc.css('div.tw-plugin-full-event-list ul li div.show_full span.show_details').each do |link|
-  shows << {
-    'show_date' => doc.css('span.show_date span.tw-event-date-complete').text,
-    'show_time' => doc.css('span.show_date span.tw-event-time-complete').text}
-    puts link.content
+show_html = doc.css('div.tw-plugin-full-event-list ul li div.show_full span.show_details')
+show_details = show_html.each_with_object([]) do |link, shows|
+  shows.push({
+    date: link.css('span.show_date span.tw-event-date-complete span.tw-event-date').children.to_s.strip,
+    time: link.css('span.show_date span.tw-event-time-complete span.tw-event-time').children.to_s.strip,
+    artists: link.css('span.show_artists ul li').children.map { |el| el.to_s.strip }
+  })
 end
 
-puts shows[0]['show_time']
+puts show_details
 
 
 
